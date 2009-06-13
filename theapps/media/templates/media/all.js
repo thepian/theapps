ce3ed0f51a84758008ea3ca8646ea2b767419a4d
Henrik Vendelbo <hvendelbo.dev@googlemail.com>
2009-06-13 14:59:23 +0100

    media/all.js

--- theapps/media/templates/media/all.js
+++ theapps/media/templates/media/all.js
@@ -1,2 +1,51 @@
-var entities = {{ entities }};
-var assets = {{ paths }};
+var Entity, Asset;
+
+(function(_Entity,_Asset){
+    
+function Entity(raw) {
+    for(var n in raw) this[n] = raw[n];
+}
+
+
+ http:// entities[path].domain + path
+ 
+
+Entity.prototype.get_absolute_path = function() {
+    return "http://%s%s" % (this.domain,this.path);
+};
+
+Entities.prototype.objects = Entity.objects = {
+    'all' : {},
+    'update' : function(all) {
+        self.all = all;
+    },
+    'get' : function(path) {
+        return Entity(this.all[path]);
+    }
+};
+
+function Asset(raw) {
+    for(var n in raw) this[n] = raw[n];
+}
+
+Asset.prototype.get_absolute_path = function() {
+    return "http://%s%s" % (this.domain,this.path);
+};
+
+Asset.prototype.objects = Asset.objects = {
+    'all': {},
+    'update' : function(all) {
+        self.all = all;
+    },
+    'get' : function(path) {
+        return Asset(this.all[path]);
+    }
+};
+
+if (!_Entity) _Entity = Entity;
+if (!_Asset) _Asset = Asset;
+})(Entity,Asset);
+
+Entity.objects.update({{ entities }});
+
+Asset.objects.update({{ paths }});
